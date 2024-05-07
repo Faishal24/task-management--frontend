916c15ef586cf1cb7127ffede7f71dafaeb649d9
Faishal <ishal.2401@gmail.com>
2024-05-07 20:46:04 +0700

    Remove dateComponent

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -2,27 +2,12 @@ import React from "react";
 import TaskList from "../components/TaskList";
 import Create from "../components/Task/Create";
 import { Typography } from "antd";
-import DateComponent from "../components/Misc/DateComponent";
 
 const { Title } = Typography;
 
 const Task = () => {
   return (
     <>
-      {/* <div className="title">
-        <Title
-          level={2}
-          style={{
-            textAlign: "Left",
-            paddingTop: "0px",
-            marginTop: "0px",
-            marginBottom: "24px",
-          }}
-        >
-          Daftar Tugas
-        </Title>
-        <DateComponent />
-      </div> */}
       <div className="container">
         <Create />
         <TaskList />
--- src/pages/Worker.jsx
+++ src/pages/Worker.jsx
@@ -3,27 +3,12 @@ import { Typography } from "antd";
 import AddWorker from "../components/Task/AddWorker";
 import WorkerList from "../components/Worker/WorkerList";
 import "../style/Worker.css";
-import DateComponent from "../components/Misc/DateComponent";
 
 const { Title } = Typography;
 
 const Worker = () => {
   return (
     <>
-      {/* <div className="title">
-        <Title
-          level={2}
-          style={{
-            textAlign: "Left",
-            paddingTop: "0px",
-            marginTop: "0px",
-            marginBottom: "24px",
-          }}
-        >
-          Daftar Karyawan
-        </Title>
-        <DateComponent />
-      </div> */}
       <div className="container">
         <div className="kiri">
           <AddWorker />
