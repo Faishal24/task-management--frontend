3caf4d79ef5a5f342950da93dc9f64e5b00165df
Faishal <ishal.2401@gmail.com>
2024-08-27 18:23:42 +0700

    Update Setting page to fetch user data from API

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import BtnLogout from '../components/BtnLogout'
+import React, { useEffect, useState } from "react";
+import BtnLogout from "../components/BtnLogout";
+import axios from "axios";
+import { Divider, List, Typography } from "antd";
 
 const Setting = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/user/manager/6608fed14eab8bd233a4f872`
+        );
+        setUser(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetch();
+  }, []);
+
   return (
     <div>
-        <BtnLogout />
+      <BtnLogout />
+      <p>{user.name}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Setting
+export default Setting;
