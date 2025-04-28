0536d4c5c29db3920b5040e93b567f2142be0244
Faishal <ishal.2401@gmail.com>
2025-04-28 22:25:35 +0700

    add tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
-  }
+  plugins: [react(), tailwindcss()],
 })
